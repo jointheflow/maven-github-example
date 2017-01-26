b2df5fcaedd1de7ea54a860eba784c1c6b9bbc38
gianluca.ragnoni <gianluca.ragnoni@gmail.com>
2017-01-26 14:46:35 +0100

    using porfile and ant plugin to run differents ant script associate with differents profile

--- src/main/java/ragnoni/maven/example/App.java
+++ src/main/java/ragnoni/maven/example/App.java
@@ -4,10 +4,12 @@ package ragnoni.maven.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+	public final static String HOST_NAME="https://app-nea-it.appspot.com";
+
+    public static void main( String[] args ){
+		
+		
         System.out.println( "Hello World 2!" );
     }
 }
